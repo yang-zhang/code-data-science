ed39c3f245f138e4d7c65b3e34dca4482379987d
yang-zhang <zhangyangcolostate@gmail.com>
2017-04-10 14:11:31 -0400

    Update notebook

--- python.py
+++ python.py
@@ -6,11 +6,41 @@
 from ds_utils.imports import *
 
 
-# ## Plot image data 
+# ### Plot image data
 
-# In[3]:
+# In[30]:
+
+plt.imshow(np.random.randn(1000, 1000, 3));
+
+
+# In[31]:
+
+plt.imshow(np.random.randn(1000, 1000));
+
+
+# In[32]:
+
+plt.imshow(np.random.randn(1000, 1000), cmap=plt.cm.gray_r);
+
+
+# In[39]:
 
 from sklearn import datasets
 digits = sklearn.datasets.load_digits()
-plt.imshow(digits.images[-1], cmap=plt.cm.gray_r)
+image = digits.images[np.random.choice(digits.images.shape[0])]
+
+
+# In[40]:
+
+plt.imshow(image)
+
+
+# In[41]:
+
+plt.imshow(image, cmap=plt.cm.gray_r)
+
+
+# In[ ]:
+
+
 
